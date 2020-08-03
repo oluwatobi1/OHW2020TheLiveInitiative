258d06f4fa28bb1b1d5974b41f215c339623f033
oluwatobi1 <akintunlesetobi@gmail.com>
2020-08-03 22:17:43 +0100

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,33 @@
+var html = '<nav class="navbar navbar-expand-lg navbar-light bg-light zone">\
+  <!-- image -->\
+  <nav class="navbar navbar-light bg-light">\
+    <a class="navbar-brand" href="#">\
+<!-- Change the brand image on the nav bar to a big & transparent one -->\
+      <img src="assests/logo.jpg" width="50" height="30" alt="">\
+    </a>\
+  </nav>\
+<a class="navbar-brand" href="#">\LIVE INITIATIVE</a>\
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+<span class="navbar-toggler-icon">\</span>\
+</button>\
+<div class="collapse navbar-collapse" id="navbarSupportedContent">\
+<ul class="navbar-nav mr-auto">\
+  <li class="nav-item active ">\
+    <a class="nav-link" href="#">\Our Programs <span class="sr-only">\(current)</span>\</a>\
+  </li>\
+  <li class="nav-item active">\
+    <a class="nav-link" href="#">\About us <span class="sr-only">\(current)</span>\</a>\
+  </li>\
+  <li class="nav-item active">\
+    <a class="nav-link" href="#">\The Brains <span class="sr-only">\(current)</span>\</a>\
+  </li>\
+</ul>\
+  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">\Contact Us</button>\
+</form>\
+</div>\
+</nav>'
+
+
+
+
+document.write(html)
